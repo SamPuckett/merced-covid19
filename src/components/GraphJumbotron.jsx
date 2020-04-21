7877004a82b43afa31206f03dcb6fe357feb5897
Samuel Ruiz <samuelpuckett77@gmail.com>
2020-04-21 14:05:16 -0700

    Updated Graph to Monthly
    - updated graph to monthly vs weekly
    - fixed some css on the mobile for the graph
    - had to change the chart's maintainAspectRation option to false in order to scale properly on mobile

--- src/components/GraphJumbotron.jsx
+++ src/components/GraphJumbotron.jsx
@@ -1,17 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { Container, Jumbotron } from "reactstrap";
 import { Line } from "react-chartjs-2";
+import "./GraphJumbotron.css";
 
 const GraphJumbotron = () => {
   const [chartData, setChartData] = useState({});
 
   const chart = () => {
     setChartData({
-      labels: ["4/11", "4/12", "4/13", "4/14", "4/15", "4/16", "4/17"],
+      labels: [
+        "4/1",
+        "4/2",
+        "4/3",
+        "4/4",
+        "4/5",
+        "4/6",
+        "4/7",
+        "4/8",
+        "4/9",
+        "4/10",
+        "4/11",
+        "4/12",
+        "4/13",
+        "4/14",
+        "4/15",
+        "4/16",
+        "4/17",
+        "4/18",
+        "4/19",
+        "4/20",
+        "4/21",
+      ],
       datasets: [
         {
           label: "Number of Postitive COVID-19 Cases",
-          data: [64, 65, 69, 74, 76, 79, 85],
+          data: [
+            12,
+            16,
+            19,
+            27,
+            29,
+            29,
+            34,
+            40,
+            52,
+            59,
+            64,
+            65,
+            69,
+            74,
+            76,
+            79,
+            85,
+            86,
+            90,
+            90,
+            92,
+          ],
           backgroundColor: ["var(--test)"],
           borderWidth: 4,
           lineTension: 0,
@@ -26,13 +71,14 @@ const GraphJumbotron = () => {
 
   return (
     <Jumbotron className='number-jumbotron'>
-      <Container style={{ width: "80vw", height: "35vw" }}>
+      <Container className='graph-jumbotron'>
         <Line
           data={chartData}
           options={{
             responsive: true,
+            maintainAspectRatio: false,
             title: {
-              text: "COVID-19 Cases in The Past 7 Days",
+              text: "COVID-19 Cases in April",
               display: true,
             },
             scales: {
@@ -40,7 +86,7 @@ const GraphJumbotron = () => {
                 {
                   ticks: {
                     autoSkip: true,
-                    maxTicksLimit: 10,
+                    // maxTicksLimit: 10,
                     beginAtZero: true,
                   },
                   gridLines: {
@@ -52,7 +98,7 @@ const GraphJumbotron = () => {
                 {
                   ticks: {
                     autoSkip: true,
-                    maxTicksLimit: 10,
+                    // maxTicksLimit: 10,
                     beginAtZero: true,
                   },
                   gridLines: {
